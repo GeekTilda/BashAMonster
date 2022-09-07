fb575171e5a5ced2720b67550ffd54ebe6ab0662
Tilda Lindblad Aderud <lindbladaderud.tilda@elev.ga.ntig.se>
2022-09-07 10:41:16 +0200

    First commit

---
+++ BashAMonster/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Monster monster1 = new Monster();
+monster1.namn = "Calle";
+monster1.farg = "Blå";
+monster1.pals = "Fluffig";
+monster1.alder = 5;
+
+Vapen vapen1 = new Vapen();
+vapen1.namn = "Spira";
+vapen1.vasshet = 5;
+
+System.Console.Write("Vad vill du döpa Monstret till? ");
+monster1.namn = Console.ReadLine();
+System.Console.WriteLine("Monstret heter " + monster1.namn);
+System.Console.WriteLine("Monstret är " + monster1.farg);
+System.Console.WriteLine("Monstret är " + monster1.pals);
+System.Console.WriteLine("Monstret är " + monster1.alder + " år gammal");
+Console.ReadLine();
+
+class Monster {
+    public string namn;
+    public string farg;
+    public string pals;
+    public int alder;
+}
+
+class Vapen {
+    public int vasshet;
+    public string namn;
+}
